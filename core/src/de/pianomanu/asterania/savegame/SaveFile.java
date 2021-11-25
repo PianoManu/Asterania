492827d327a3e7d443ed89d3034d7980dbacf24e
Manuel <jirsakm@web.de>
2021-11-25 20:48:14 +0100

    Added SaveFile logger plus warning if directory already exists

--- core/src/de/pianomanu/asterania/savegame/SaveFile.java
+++ core/src/de/pianomanu/asterania/savegame/SaveFile.java
@@ -1,10 +1,14 @@
 package de.pianomanu.asterania.savegame;
 
+import de.pianomanu.asterania.AsteraniaMain;
 import de.pianomanu.asterania.world.World;
 
 import java.io.File;
+import java.util.logging.Logger;
 
 public class SaveFile {
+    private static final Logger LOGGER = AsteraniaMain.getLogger();
+
     private final String name;
     private Universe universe = null;
     private World homeWorld;
@@ -12,7 +16,12 @@ public class SaveFile {
     public SaveFile(String name) {
         this.name = name;
 
-        new File(name).mkdir();
+        if (new File(name).mkdir()) {
+            LOGGER.info("Created directory " + name + " as save directory!");
+        } else {
+            LOGGER.severe("Could not create directory " + name + ". Does it exist already?");
+            LOGGER.severe("You should change the name or move the directory before. Otherwise, your existing files might become corrupted or destroyed");
+        }
     }
 
     public void loadUniverse() {
