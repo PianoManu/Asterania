e42b8211e305cf95cdf56768951f742668230c42
Manuel <jirsakm@web.de>
2022-09-02 16:44:24 +0200

    Deleted now unused DecorationTile class

--- core/src/de/pianomanu/asterania/world/tile/DecorationTile.java
+++
@@ -1,8 +0,0 @@
-package de.pianomanu.asterania.world.tile;
-
-public class DecorationTile extends Tile {
-    public DecorationTile(String name, TileSettings settings) {
-        super(name, settings);
-    }
-
-}
