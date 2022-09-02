c7458d7f4db6723edf78e13cbdac0418ef5fa562
Manuel <jirsakm@web.de>
2022-09-02 16:48:22 +0200

    Moved some textures from tiles to decoration

--- core/src/de/pianomanu/asterania/render/DecorationLayerRenderer.java
+++ core/src/de/pianomanu/asterania/render/DecorationLayerRenderer.java
@@ -6,19 +6,17 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import de.pianomanu.asterania.AsteraniaMain;
 import de.pianomanu.asterania.config.DisplayConfig;
 import de.pianomanu.asterania.world.tile.Tile;
-import de.pianomanu.asterania.world.worldsections.WorldSection;
 
 public class DecorationLayerRenderer {
-    public static void addDecorations(SpriteBatch batch, WorldSection section, int x, int y, int xTile, int yTile) {
-        Tile decoration = section.getDecorationLayerTileAbsoluteCoordinates(x, y);
+    public static void addDecorations(SpriteBatch batch, Tile decoration, int xTile, int yTile) {
         int numberOfTextures = decoration.getNumberOfDifferentTextures();
         if (numberOfTextures == 1)
-            batch.draw(decoration.getTexture(AsteraniaMain.assetManager.get(Atlases.TILE_ATLAS_LOCATION, TextureAtlas.class)), xTile, yTile, DisplayConfig.TILE_SIZE, DisplayConfig.TILE_SIZE);
+            batch.draw(decoration.getTexture(AsteraniaMain.assetManager.get(Atlases.DECORATION_ATLAS_LOCATION, TextureAtlas.class)), xTile, yTile, DisplayConfig.TILE_SIZE, DisplayConfig.TILE_SIZE);
         else {
             //pseudo-random selection of stone coast texture using x and y position
             //x*3, y*5+1: prevent patterns
-            int textureNumber = (((x * 3 % numberOfTextures) + (y * 5 + 1 % numberOfTextures)) % numberOfTextures) + 1; //add 1 for intervall [1, 4]
-            TextureRegion decorationTexture = AsteraniaMain.assetManager.get(Atlases.TILE_ATLAS_LOCATION, TextureAtlas.class).findRegion(decoration.getSaveFileString() + textureNumber);
+            int textureNumber = (((xTile * 3 % numberOfTextures) + (yTile * 5 + 1 % numberOfTextures)) % numberOfTextures) + 1; //add 1 for intervall [1, 4]
+            TextureRegion decorationTexture = AsteraniaMain.assetManager.get(Atlases.DECORATION_ATLAS_LOCATION, TextureAtlas.class).findRegion(decoration.getSaveFileString() + textureNumber);
             batch.draw(decorationTexture, xTile, yTile, DisplayConfig.TILE_SIZE, DisplayConfig.TILE_SIZE);
         }
     }
