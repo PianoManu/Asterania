389a2a0cbda105f3af5eb4dd75f0dc9ed5c35278
Manuel <jirsakm@web.de>
2022-09-03 21:53:14 +0200

    Added SpriteBatch and ShapeRenderer to main instance

--- core/src/de/pianomanu/asterania/AsteraniaMain.java
+++ core/src/de/pianomanu/asterania/AsteraniaMain.java
@@ -2,6 +2,8 @@ package de.pianomanu.asterania;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import de.pianomanu.asterania.screens.LoadingScreen;
 import de.pianomanu.asterania.utils.logging.LoggerUtils;
 import de.pianomanu.asterania.utils.savegame.Savegame;
@@ -13,7 +15,10 @@ import java.util.logging.Logger;
 public class AsteraniaMain extends Game {
 
 	public static AsteraniaMain INSTANCE;
-	public static AssetManager assetManager;
+	public static AssetManager assetManager;//TODO make not static
+
+	private final ShapeRenderer shapeRenderer;
+	private final SpriteBatch batch;
 
 	public static final Level LOG_LEVEL = Level.FINE;
 	private static final Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
@@ -22,7 +27,11 @@ public class AsteraniaMain extends Game {
 
 	public AsteraniaMain() {
 		INSTANCE = this;
+
 		LOGGER.setLevel(LOG_LEVEL);
+
+		this.shapeRenderer = new ShapeRenderer();
+		this.batch = new SpriteBatch();
 		LogManager.getLogManager().getLogger(Logger.GLOBAL_LOGGER_NAME).setLevel(LOG_LEVEL);
 	}
 
@@ -39,5 +48,13 @@ public class AsteraniaMain extends Game {
 
 	public static Logger getLogger() {
 		return LOGGER;
+	}
+
+	public ShapeRenderer getShapeRenderer() {
+		return this.shapeRenderer;
+	}
+
+	public SpriteBatch getBatch() {
+		return this.batch;
 	}
 }
