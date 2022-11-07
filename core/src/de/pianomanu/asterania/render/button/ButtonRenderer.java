1e46b6f99b87496075bb67d1099ed3bf93c5430c
Manuel <jirsakm@web.de>
2022-11-07 13:17:37 +0100

    Added hovering method to ButtonRenderer

--- core/src/de/pianomanu/asterania/render/button/ButtonRenderer.java
+++ core/src/de/pianomanu/asterania/render/button/ButtonRenderer.java
@@ -1,8 +1,11 @@
 package de.pianomanu.asterania.render.button;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import de.pianomanu.asterania.AsteraniaMain;
+import de.pianomanu.asterania.render.ShapeRendererUtils;
 import de.pianomanu.asterania.render.SpriteBatchUtils;
 import de.pianomanu.asterania.render.atlas.Atlases;
 import de.pianomanu.asterania.render.text.TextRenderer;
@@ -14,6 +17,7 @@ public class ButtonRenderer {
         for (Button b :
                 buttons) {
             renderButton(b);
+            renderHovering(b);
         }
     }
 
@@ -21,5 +25,16 @@ public class ButtonRenderer {
         TextureRegion buttonTexture = AsteraniaMain.INSTANCE.getAssetManager().get(Atlases.BUTTON_ATLAS_LOCATION, TextureAtlas.class).findRegion(button.getTextureName());
         SpriteBatchUtils.getInstance().draw(buttonTexture, (int) button.getStart().x, (int) button.getStart().y, (int) button.getFormat().x, (int) button.getFormat().y);
         TextRenderer.getInstance().renderText((int) (button.getStart().x + button.getFormat().x / 2), ((int) (button.getStart().y + button.getFormat().y / 2)), button.getButtonText().getTextValue());
+    }
+
+    public static void renderHovering(Button button) {
+        int mouseX = Gdx.input.getX();
+        int mouseY = Gdx.graphics.getHeight() - Gdx.input.getY();
+
+        ShapeRendererUtils.enableTransparency();
+        if (mouseX >= button.getStart().x && mouseY >= button.getStart().y && mouseX <= button.getEnd().x && mouseY <= button.getEnd().y) {
+            ShapeRendererUtils.getInstance().rect(button.getStart().x, button.getStart().y, button.getFormat().x, button.getFormat().y, new Color(1, 1, 1, 0.2f));
+        }
+        ShapeRendererUtils.disableTransparency();
     }
 }
